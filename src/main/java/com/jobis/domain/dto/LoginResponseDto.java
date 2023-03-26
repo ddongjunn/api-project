2bf3f7211cb87b0837fe53888a83ad70dc1b0155
djlee <ddongjunn@gmail.com>
2023-03-26 23:56:09 +0900

    feat: signup test 작성

--- src/main/java/com/jobis/domain/dto/LoginResponseDto.java
+++ src/main/java/com/jobis/domain/dto/LoginResponseDto.java
@@ -1,6 +1,7 @@
 package com.jobis.domain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Builder
 public class LoginResponseDto {
 
     @Schema(description = "토큰", example = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJraW0iLCJleHAiOjE2Nzk4NDUyMTQsImlhdCI6MTY3OTgwOTIxNH0.iG0X9JB_FunwRhGxd6ruaZv_y3_cJvKIe7Gv2BNDEMI22igci2pnEIGNZQC9BwRyJd133IJHbHLCSiGSCLVm2g")
---
+++ src/test/java/com/jobis/controller/SzsControllerTest.java
@@ -0,0 +1,73 @@
+package com.jobis.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jobis.common.utils.TokenProvider;
+import com.jobis.domain.dto.LoginResponseDto;
+import com.jobis.domain.dto.SignUpRequestDto;
+import com.jobis.domain.dto.SignUpResponseDto;
+import com.jobis.repository.MemberRepository;
+import com.jobis.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class SzsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private MemberService memberService;
+
+    @Test
+    void signup() throws Exception {
+        SignUpResponseDto signUpResponseDto = SignUpResponseDto.builder()
+                .userId("hong")
+                .joinDate(LocalDateTime.now())
+                .build();
+
+        given(memberService.join(any())).willReturn(signUpResponseDto);
+
+        SignUpRequestDto signUpRequestDto = SignUpRequestDto.builder()
+                .userId("hong")
+                .password("1234")
+                .name("홍길동")
+                .regNo("860824-1655068")
+                .build();
+        String requestBody = objectMapper.writeValueAsString(signUpRequestDto);
+
+        mockMvc.perform(post("/szs/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(signUpResponseDto.getUserId()));
+    }
+
+    @Test
+    void login() throws Exception {
+    }
+}
