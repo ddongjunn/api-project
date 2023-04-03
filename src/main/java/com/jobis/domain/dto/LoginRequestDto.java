45aad5705f4bcf7e21cc68c0d04e206930ffa56e
dongjun lee <ddongjunn@gmail.com>
2023-04-03 09:55:54 +0900

    fix: 오류개선

--- src/main/java/com/jobis/domain/dto/LoginRequestDto.java
+++ src/main/java/com/jobis/domain/dto/LoginRequestDto.java
@@ -1,35 +1,24 @@
 package com.jobis.domain.dto;
 
-<<<<<<< HEAD
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-=======
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
->>>>>>> 3f7faa693bab81d752a5d73620b743a73b93943f
 
 import javax.validation.constraints.NotBlank;
 
 @Schema(name = "LoginRequestDto", description = "로그인 요청")
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
-<<<<<<< HEAD
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-=======
 @NoArgsConstructor
->>>>>>> 3f7faa693bab81d752a5d73620b743a73b93943f
 public class LoginRequestDto {
 
 
     @Schema(description = "아이디",  example = "hong")
-    @NotBlank
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
 
     @Schema(description = "비밀번호", example = "1234")
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
